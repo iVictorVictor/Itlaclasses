828cc51a4558ce97b61df9c77b759d4a131d3c59
iVictorVictor <victorvicman09@gmail.com>
2025-10-11 23:17:13 -0400

    ITLA

---
+++ homework1/Homeworkone/Program.cs
@@ -0,0 +1,14 @@
+﻿int num;
+//Victor Victor Aybar 20250963 
+Console.Write("Could you insert a number to determine if is even or not even \n");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("The number that has been inserted is even");
+}
+else
+{
+    Console.WriteLine("The number that has been inserted is not even");
+}
+Console.ReadKey();
