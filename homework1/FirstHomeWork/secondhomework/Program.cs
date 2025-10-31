6116f9ec50a5a8fa8ce82fc4e6faf03badcad2ef
iVictorVictor <victorvicman09@gmail.com>
2025-10-30 23:23:15 -0400

    Classes

---
+++ homework1/FirstHomeWork/secondhomework/Program.cs
@@ -0,0 +1,19 @@
+﻿double num1 = 6, num2 = 3;
+
+double sum = num1 + num2;
+
+double substraction = num1 - num2;
+
+double multiplication = num1 * num2;
+
+double division = 10.0 / 9.0;
+
+double module = 10 % 9;
+
+Console.WriteLine("sum = " + sum);
+Console.WriteLine("\nsubstraction = " + substraction);
+Console.WriteLine("\nmultiplication = " + multiplication);
+Console.WriteLine("\ndivision = " + division);
+Console.WriteLine("\nmodule = " + module);
+
+//VictorVictor 20250963
